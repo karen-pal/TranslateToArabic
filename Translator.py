b065198b08723e1f76a8f301b97b0275ac05675a
Karen Palacio <karen.palacio.1994@gmail.com>
2020-06-04 22:56:20 -0300

    add german, cleanup

--- Translator.py
+++ Translator.py
@@ -7,26 +7,33 @@ from time import sleep
 import click
 
 LANGUAGES = {
-    "turkish":"https://www.freetranslations.org/english-to-turkish-translation.html",
-    "arab":"https://www.freetranslations.org/english-to-arabic-translation.html",
-    "greek":"https://www.freetranslations.org/english-to-greek-translation.html",
-    "hindi":"https://www.freetranslations.org/english-to-hindi-translation.html",
+    "turkish": "https://www.freetranslations.org/english-to-turkish-translation.html",
+    "arab": "https://www.freetranslations.org/english-to-arabic-translation.html",
+    "greek": "https://www.freetranslations.org/english-to-greek-translation.html",
+    "hindi": "https://www.freetranslations.org/english-to-hindi-translation.html",
+    "spanish": "https://www.freetranslations.org/english-to-spanish-translation.html",
+    "german": "https://www.freetranslations.org/english-to-german-translation.html",
 }
 
+
 class Translator:
-    def __init__(self,language, browser="Firefox", headless=False):
+    def __init__(self, language, browser="Firefox", headless=False):
         options = Options()
-        if headless: options.headless = True
+        if headless:
+            options.headless = True
         if browser == "Firefox":
             from selenium.webdriver import Firefox
+
             self._br = Firefox()
         self._br.get(LANGUAGES[language])
         # self._br.set_page_load_timeout(25)
 
     def translate(self, string):
-        if not string: return "Done"
+        if not string:
+            return "Done"
         for i in range(3):
-            try: return self._translate(string.replace("\\", ''))
+            try:
+                return self._translate(string.replace("\\", ""))
             except Exception as e:
                 print(e)
                 sleep(2)
@@ -38,18 +45,28 @@ class Translator:
         tag.send_keys(Keys.DELETE)
         tag.send_keys(string)
         self._br.find_element_by_class_name("translate-form-control").click()
-        WebDriverWait(self._br, 10).until(ec.presence_of_element_located((By.CLASS_NAME, "mttextarea")))
+        WebDriverWait(self._br, 10).until(
+            ec.presence_of_element_located((By.CLASS_NAME, "mttextarea"))
+        )
         return self._br.find_element_by_xpath("//*[@id='TranslationOutput']/div").text
 
-    def reverse(self,string):
+    def reverse(self, string):
         tag = self._br.find_element_by_id("LangPair_SwapImg")
         tag.click()
+        tag = self._br.find_element_by_id("InputText")
+        tag.send_keys(Keys.CONTROL + "a")
+        tag.send_keys(Keys.DELETE)
         return self.translate(string)
 
     def close(self):
         self._br.close()
 
+
 @click.command
-@click.option('--browser', default="Firefox", help='browser to run the script to. Options: Firefox, Chrome')
+@click.option(
+    "--browser",
+    default="Firefox",
+    help="browser to run the script to. Options: Firefox, Chrome",
+)
 def translator(browser):
     return Translator()
--- echo.py
+++ echo.py
@@ -3,8 +3,8 @@ import random
 
 to_translate = [
     "my back against the cavern walls, the emptiness results in echoes, the absence of connection, pricking my skin",
-    "my back hitting the cavern walls, the echoes that arise from the emptiness, no connection, pricking my skin",
-    "i can feel the cavern wall surface upon my skin, i can hear the echoes filling the emptiness, null connection, pricking my skin",
+    "hitting the cavern walls, the echoes that arise from the emptiness, no connection, pricking my skin",
+    "i can feel the cavern wall surface upon my skin and i can hear the echoes filling the emptiness, null connection, tearing my skin apart",
     "the skin feels the cavern wall, the ear is filled with emptiness' echoes, no connection, the skin feels pain",
     "the cavern wall is felt by the skin, the echoes are heard by the ear, connection doesn't exist, pain is felt by the skin",
 ]
@@ -14,32 +14,39 @@ class InsideCave:
     def __init__(self, *, resonance):
         self.poems = to_translate
         self.resonance = resonance
-        self.languages = ["turkish","arab","hindi","greek"]
+        self.languages = [
+            # "turkish",
+            # "arab",
+            # "hindi",
+            # "greek",
+            # "spanish",
+            "german",
+        ]
 
     def echo(self, out_file, language):
         tr = Translator(language)
         for i in range(self.resonance):
             text = random.choice(self.poems)
-            translated_to_arab = tr.translate(text)
-            english_again = tr.reverse(translated_to_arab)
+            translated_to_another = tr.translate(text)
+            english_again = tr.reverse(translated_to_another)
 
             print(text)
-            print(translated_to_arab)
+            print(translated_to_another)
             print(english_again)
             out_file.write("\n")
             out_file.write(text)
             out_file.write("\n")
-            out_file.write(translated_to_arab)
+            out_file.write(translated_to_another)
             out_file.write("\n")
             out_file.write(english_again)
             out_file.write("\n")
-            print(i, " [finished writting to file]")
+            print(i, " [i have finished writting to file]")
             if english_again not in self.poems:
                 self.poems.append(english_again)
         tr.close()
 
     def propagate(self):
         for i in range(len(self.languages)):
-            results_file = open("results_echo_" + self.languages[i] + ".txt", "w")
+            results_file = open("results_echo_2_" + self.languages[i] + ".txt", "w")
             self.echo(results_file, self.languages[i])
             results_file.close()
